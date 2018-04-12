bc6d49320d5a14bee599d2925601d73084a70eef
markcarrmusic@gmail.com <markcarrmusic@gmail.com>
2018-04-12 17:09:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,13 +19,16 @@ function logWhisper(string) {
 }
 
 // 6. sayHiToGrandma function
-function sayHiToGrandma(string) {
+function sayHiToGrandma(string)
+  // 6a. Grandma can hear you {
   if (string === shout(string)) {
     return "YES INDEED!"
   }
+  // 6b. Grandma can't hear you
   else if (string === whisper(string)) {
     return "I can't hear you!"
   }
+  // 6c. I love you grandma
   else if (string = "I love you, Grandma") {
     return "I love you, too." 
   }
