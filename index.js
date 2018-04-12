49f2fb0b66dad14a6e3168ffabdfe0db70a5c1dc
markcarrmusic@gmail.com <markcarrmusic@gmail.com>
2018-04-12 17:06:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ mixedCase.toUpperCase() === mixedCase // false: the string is not all uppercase
 
 
 // 6. sayHiToGrandma function
-function sayHiToGrandmma(string) {
+function sayHiToGrandma(string) {
   if (string === shout(string)) {
-    return "YES INDEED!";
+    return "YES INDEED!"
   }
   else if (string === whisper(string)) {
-    return "I can't hear you!";
+    return "I can't hear you!"
   }
   else if (string === "I love you, Grandma") {
-    return "I love you, too." ;
+    return "I love you, too." 
   }
 }
